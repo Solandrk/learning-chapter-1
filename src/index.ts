6e16a1c3333a7c6aa088b47e9d3ab62add7c0696
Solandrk <ranaee2000@gmail.com>
2025-05-18 15:44:45 +0330

    add ai

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,21 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// import OpenAI from 'openai';
+// const client = new OpenAI({apiKey:''});
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const response = await env.AI.run('@cf/meta/llama-3-8b-instruct', {
+			messages: [
+				{
+					role: 'user',
+					content: 'Hello, how are you?',
+				},
+			],
+		});
+		console.log(response);
+
+		return new Response();
 	},
 } satisfies ExportedHandler<Env>;
